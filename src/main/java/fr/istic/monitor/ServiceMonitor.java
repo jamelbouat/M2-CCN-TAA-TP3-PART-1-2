a1b138bd00efb35a5b89ac675b92a62ad124f125
djamel <djamel.bouatmane@etudiant.univ-rennes1.fr>
2020-10-25 13:38:57 +0100

    added AOP with @Before on method call

--- src/main/java/fr/istic/monitor/ServiceMonitor.java
+++ src/main/java/fr/istic/monitor/ServiceMonitor.java
@@ -1,10 +1,24 @@
 package fr.istic.monitor;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class ServiceMonitor {
 
+	@Before("execution(* fr.istic.service.*.*.*(..))")
+	public void logServiceAccess(JoinPoint joinPoint) {
+		
+		System.out.println("\nClass Name = " + joinPoint.getTarget().getClass().getName());
+		System.out.println("Called method = " + joinPoint.getSignature().getName());
+	}
+
+//	@AfterReturning("execution(* fr.istic.service.store.Store.*(..))")
+//	public void logServiceAccess(JoinPoint joinPoint) {
+//		System.out.println("Completed: " + joinPoint);
+//	}
 }
