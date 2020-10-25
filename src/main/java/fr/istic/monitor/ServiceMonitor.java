1375aa923d0f5d879f63983e420f30a9c3630fbb
djamel <djamel.bouatmane@etudiant.univ-rennes1.fr>
2020-10-25 16:02:09 +0100

    added security on pay method, do not accept null values

--- src/main/java/fr/istic/monitor/ServiceMonitor.java
+++ src/main/java/fr/istic/monitor/ServiceMonitor.java
@@ -1,7 +1,9 @@
 package fr.istic.monitor;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -15,6 +17,19 @@ public class ServiceMonitor {
 		
 		System.out.println("\nClass Name = " + joinPoint.getTarget().getClass().getName());
 		System.out.println("Called method = " + joinPoint.getSignature().getName());
+	}
+	
+	@Around("execution(* fr.istic.service.store.Store.pay(..))")
+	public boolean checkForNullAccountIds(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		Object[] args = proceedingJoinPoint.getArgs();
+		
+		for (Object arg : args) {
+			if (arg == null)
+				return false;
+		}
+		
+		proceedingJoinPoint.proceed();
+		return true;
 	}
 
 //	@AfterReturning("execution(* fr.istic.service.store.Store.*(..))")
